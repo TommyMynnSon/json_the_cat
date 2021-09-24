04bbca89c1e65a78a989a9ca03411ea06e3b1251
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-23 19:58:36 -0700

    Fix bug related to indexing an empty array

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,18 +3,27 @@ const request = require('request');
 
 // Fetch data from URL and pass it to the provided callback function
 const fetchBreedDescription = (breedName, callback) => {
-  const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
 
   request(URL, (error, response, body) => {
+    // If request returned an error
+    if (error) {
+      callback(error, null);
+
+      return;
+    }
 
     const data = JSON.parse(body);
 
+    // If request did not return an error AND breedName is an invalid breed
     if (data.length === 0) {
-      console.log(`Search query for breed '${breedName}' not found!`);
-      process.exit();
+      callback(`Search query for '${breedName}' not found!`, null);
+
+      return;
     }
 
-    callback(error, data[0].description);
+    // If request did not return an error and breedName is a valid breed
+    callback(null, data[0].description);
   });
 };
 
